d98fa8e8dbe7d2badbfce589c9ed05373455efd0
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 09:51:34 -0600

    Initial commit

---
+++ app/src/main/java/android7t11/misael/itics/tesoem/edu/mx/android7t11/android7t11.java
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+package android7t11.misael.itics.tesoem.edu.mx.android7t11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class android7t11 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android7t11);
+    }
+}
---
+++ app/src/test/java/android7t11/misael/itics/tesoem/edu/mx/android7t11/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package android7t11.misael.itics.tesoem.edu.mx.android7t11;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
